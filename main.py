bb506a587d1ca7c3ab8c9556b7a40929a17b5f8c
yair_zohav <yairzohav@gmail.com>
2020-12-17 22:21:44 +0200

    Main started

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import gym
 
+from methods import SarsaAlgorithm
 
 if __name__ == '__main__':
     eps = 0.25
@@ -7,9 +8,16 @@ if __name__ == '__main__':
     alphas = [0.01, 0.1]
     gamma = 0.95
     env = gym.make('MountainCar-v0')
-
-    env.reset()
-    for _ in range(10000):
-        env.render()
-        env.step(env.action_space.sample())  # take a random action
+    env._max_episode_steps = 500
+    algorithm = SarsaAlgorithm(eps, _lambda=0.5,
+                               alpha=0.02,
+                               gamma=1,
+                               sigma_p=0.04,
+                               sigma_v=0.0004,
+                               feature_space=96,
+                               plot_step=15e3)
+    step_list, avg_reward, avg_MC = algorithm.sarsa(env)
+    # show_and_save_plot(f'lambda: {_lambda} | alpha: {alpha}',step_list, avg_reward,"Average reward")
+    # show_and_save_plot(f'(MC) lambda: {_lambda} | alpha: {alpha}',step_list, avg_MC,"Average MC reward")
+    # simulate(env, algorithm.epsilon_greedy, gamma, episodes=1)
     env.close()
--- methods.py
+++ methods.py
@@ -1,7 +1,11 @@
+import gym
 import numpy as np
 from itertools import product
 from random import uniform
 from numpy import diag, array, exp
+from collections import defaultdict, Counter
+from random import random as rnd
+from Utils import flush_print, simulate, padding_theta, argmax
 
 
 def create_x(p, v):
@@ -15,13 +19,17 @@ def create_x(p, v):
     c_p = [uniform(a=-1.2, b=0.6) for _ in range(4)]
     c_v = [uniform(a=-0.07, b=0.07) for _ in range(8)]
     c = product(c_p, c_v)
-    return array([array([p, v]) - array([c1, c2]) for c1, c2 in c]).T
+    return array([array([p, v]) - array([c1, c2]) for c1, c2 in c])
 
 
-def create_theta(sigma_p, siga_v):
+def create_theta(sigma_p, sigma_v):
+    feature_map = {}
+
     def theta(p, v):
-        X = create_x(p, v)
-        return exp(-(X.T @ (1 / diag([sigma_p, siga_v])) @ X) / 2)
+        if (p, v) not in feature_map:
+            X = create_x(p, v)
+            feature_map[p, v] = exp(-(X * X @ (1 / array([sigma_p, sigma_v]))) / 2)
+        return feature_map[p, v]
 
     return theta
 
@@ -30,8 +38,76 @@ class Q:
     def __init__(self, W):
         self.W = W
 
-    def calc(self, p, v, theta):
+    def calc(self, theta):
         return theta.T @ self.W
 
+    def __call__(self, theta):
+        return self.calc(theta)
 
-create_x(0.2, 0.05)
+    def update_parameters(self, dw):
+        self.W += dw
+
+
+ACTION_SPACE = [-1, 0, 1]
+
+
+def create_epsilon_greedy_policy(Q, epsilon):
+    def epsilon_greedy(theta, isGreedy=False):
+        eps = 0 if isGreedy else epsilon
+        best_greedy_action = argmax(Q, theta, ACTION_SPACE)
+        rand_number = rnd()
+        if rand_number <= eps / len(ACTION_SPACE) + 1 - eps:
+            return best_greedy_action
+        return np.random.choice(array([a for a in range(len(ACTION_SPACE)) if a != best_greedy_action]))
+
+    return epsilon_greedy
+
+
+class SarsaAlgorithm:
+
+    def __init__(self, eps, _lambda, alpha, gamma, sigma_p, sigma_v, feature_space=96, plot_step=15e3):
+        self.Q = Q(W=np.random.rand(feature_space))
+        self.feature_space = feature_space
+        self.epsilon_greedy = create_epsilon_greedy_policy(self.Q, eps)
+        self._lambda = _lambda
+        self.alpha = alpha
+        self.gamma = gamma
+        self.plot_step = plot_step
+        self.theta = create_theta(sigma_p, sigma_v)
+
+    def sarsa(self, env, max_total_steps=int(1e6)):
+        steps = 0
+        avg_list = []
+        step_list = []
+        avg_MC_list = []
+        while steps <= max_total_steps:
+            flush_print(f'\rSarsa training process {100 * steps // max_total_steps}%')
+            state = env.reset()
+            E = np.zeros(self.feature_space)
+            theta = self.theta(*state)
+            action = self.epsilon_greedy(theta)
+            padded_theta = padding_theta(theta, action)
+            for _ in range(200):
+                steps += 1
+                next_state, R, done, info = env.step(action)  # take a random action
+                next_theta = self.theta(*next_state)
+                new_action = self.epsilon_greedy(next_theta)
+                next_padded_theta = padding_theta(next_theta, new_action)
+                delta = R + self.gamma * self.Q(next_padded_theta) - self.Q(padded_theta)
+                E = self._lambda * self.gamma * E + padded_theta
+                self.Q.update_parameters(dw=self.alpha * delta * E)
+                action = new_action
+                padded_theta = next_padded_theta
+                # Collect information for the plot.
+                # if not steps % self.plot_step:
+                #     avg_reward, avg_MC_reward = simulate(env, self.epsilon_greedy, self.gamma, episodes=100)
+                #     avg_list.append(avg_reward)
+                #     avg_MC_list.append(avg_MC_reward)
+                #     step_list.append(steps)
+
+                if done:
+                    break
+
+        flush_print(f'\rSarsa training process {100}%')
+        return step_list, avg_list, avg_MC_list
+
